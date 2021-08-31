ba1987b01b9874c801b1490c670e1bd559a68a52
Michael Cooper <mcooper6@student.rccd.edu>
2021-08-30 21:14:06 -0700

    Hello World
    Created folders and introlab

---
+++ Class/README copy.md
@@ -0,0 +1,2 @@
+# Cooper_Michael_CSC17A_48591
+CIS 17A C++ Programming - Dr. Lehr
---
+++ Lab/IntroLab/17a_Hello_World/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Michael Cooper
+ * Created on August 30, 2021, 9:05PM
+ * Purpose: Hello World
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output Library
+using namespace std;  //STD Name-space where Library is compiled
+
+//User Libraries
+
+//Global Constants not Variables
+//Math/Physics/Science/Conversions/Dimensions
+
+//Function Prototypes
+
+//Code Begins Execution Here with function main
+int main(int argc, char** argv) {
+    //Set random number seed once here
+    
+    //Declare variables here
+    
+    //Initialize variables here
+    
+    //Map inputs to outputs here, i.e. the process
+    
+    //Display the results
+    cout<<"Hello World!";
+
+    return 0;
+}
+
