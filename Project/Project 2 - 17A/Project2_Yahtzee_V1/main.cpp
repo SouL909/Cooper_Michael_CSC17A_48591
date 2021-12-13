f576c117d6c5dbad9478138c670b8fb11a887c3b
Michael Cooper <mcooper6@student.rccd.edu>
2021-12-13 00:45:40 -0800

    Changes for Project

---
+++ Project/Project 2 - 17A/Project2_Yahtzee_V1/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * File:   main.cpp
+ * Author: michaelcooper
+ * Created on December 5, 2021, 10:07 PM
+ * Project 2: Yahtzee V1
+ * Making basic outline for project and identifying new changes
+ */
+
+#include <cstdlib>
+#include "Yahtzee.h"
+using namespace std;
+
+//I want to make the main as short as possible to fully utilize the other sections 
+//and apply as many requirements. Included the main turn functions of the game to start with
+//still need to tweak and include other files, so this version does not run.
+
+int main(int, char**) {
+    Yahtzee yhtz;
+    yhtz.play();
+    return 0;
+}
+
